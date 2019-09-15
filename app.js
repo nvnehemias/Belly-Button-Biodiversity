8821c09be7660ce015e8bf864636c6d654096c76
Nehemias Vicente <nehemiasvicente@nehemiass-mbp-2.home>
2019-09-15 13:48:00 -0700

    Completed bar graph

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-function buildcharts() {
+//Create a default page 
+
+function buildropdown() {
     //Building the dropdown menu to choose id's
     d3.json("data/samples.json").then(function(data) {
         var listid = data.names;
@@ -17,8 +19,8 @@ function buildcharts() {
 function handleSubmit() {
     d3.json("data/samples.json").then(function(data) {
         var idchosen = d3.selectAll("#selDataset");
-        var dataid  = idchosen.property("value")
-        var dataid = parseInt(dataid)
+        var dataidstring  = idchosen.property("value")
+        var dataid = parseInt(dataidstring)
         var datalist = data.metadata;
         d3.selectAll("p").text("")
         for (var i=0;i< datalist.length;i++){
@@ -26,7 +28,6 @@ function handleSubmit() {
                 var chosendata = [{"id":datalist[i].id},{"ethnicity":datalist[i].ethnicity},{"gender":datalist[i].gender},{"age":datalist[i].age},{"location":datalist[i].location},{"bbtype":datalist[i].bbtype},{"wfreq":datalist[i].wfreq}];
                 var selection1 = d3.selectAll("#sample-metadata").selectAll("p")
                                 .data(chosendata);
-                console.log(chosendata)
                 selection1.enter()
                         .append("p")
                         .merge(selection1)
@@ -34,13 +35,35 @@ function handleSubmit() {
                 break
             }
         }
+        var sampleslist = data.samples
+        createTable(dataidstring,sampleslist);
     })
 }
 
+function createTable(chosen,list) {
+    console.log("this is test for new function");
+    //We are looking through so see wich samples will be used when matched with the id chosen
+    for (var i = 0;i<list.length;i++){
+        if (chosen === list[i].id){
+            var bar_chart_value = list[i].sample_values.slice(0,10);
+            var otu_ids = list[i].otu_ids.slice(0,10);
+            var otu_ids = otu_ids.map(value => "OTU "+value)
+            var otu_labels = list[i].otu_labels.slice(0,10);
+            console.log(otu_labels);
+            var data = [{
+                type: "bar",
+                x : bar_chart_value.reverse(),
+                y : otu_ids.reverse(),
+                orientation: 'h'
+            }]
+            Plotly.newPlot('bar', data)
+        }
+    }
+    //console.log(`This is the list chosen ${list}`);
+    //for (var i=0;i< datalist.length;i++){
+};
 
-
-
-buildcharts();
+buildropdown();
 /*
 
 var trace1 = {
